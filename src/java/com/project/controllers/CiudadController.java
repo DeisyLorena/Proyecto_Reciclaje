a81712dc4415982962d0d0fd9cacfdf2a7cda41f
DeisyLorena <dlnavia1@misena.edu.co>
2015-09-15 15:37:21 -0500

    Proyecto actualizado y funcionando

--- src/java/com/project/controllers/CiudadController.java
+++ src/java/com/project/controllers/CiudadController.java
@@ -11,66 +11,67 @@ import javax.faces.bean.ViewScoped;
 
 @ManagedBean
 @ViewScoped
-public class CiudadController implements Serializable { 
-     @EJB
+public class CiudadController implements Serializable {
+
+    @EJB
     private CiudadSession ciudadSession;
- 
+
     private Ciudad selectedCiudad;
-     private int idDepartamento;
+    private int idDepartamento;
     private List<Ciudad> itemsCiudad = null;
-    
-     public CiudadController() {        
+
+    public CiudadController() {
     }
-    
-    public Ciudad getSelectedCiudad(){ // para retornar datos de tipo departamentos.
-        if(selectedCiudad == null) {
+
+    public Ciudad getSelectedCiudad() { // para retornar datos de tipo departamentos.
+        if (selectedCiudad == null) {
             selectedCiudad = new Ciudad();
         }
         return selectedCiudad;
     }
-    
-    public void setSelectedCiudad(Ciudad selectedCiudad){ // para recibir un parametro de tipo departamento al cual se le nombre como selectedDepartamento. y luego modificarlo e igualarlo ala variable q se siente alamacenada.
+
+    public void setSelectedCiudad(Ciudad selectedCiudad) { // para recibir un parametro de tipo departamento al cual se le nombre como selectedDepartamento. y luego modificarlo e igualarlo ala variable q se siente alamacenada.
         this.selectedCiudad = selectedCiudad;
     }
-    
-     public int getIdDepartamento() {
+
+    public int getIdDepartamento() {
         return idDepartamento;
     }
 
     public void setIdDepartamento(int idDepartamento) {
         this.idDepartamento = idDepartamento;
     }
-    
-    public CiudadSession getCiudadSession(){
+
+    public CiudadSession getCiudadSession() {
         return ciudadSession;
     }
-    
-    public List<Ciudad> getItemsCiudad(){
-        if(itemsCiudad == null){
+
+    public List<Ciudad> getItemsCiudad() {
+        if (itemsCiudad == null) {
             try {
                 itemsCiudad = getCiudadSession().findAll();
-            } catch(Exception ex) {
-                System.err.println(ex.getMessage());   
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
             }
         }
         return itemsCiudad;
     }
-    
+
     public void create() {
         try {
             selectedCiudad.setIdDepartamento(new Departamento(idDepartamento));
             getCiudadSession().create(selectedCiudad);
-        } catch (Exception ex){
+        } catch (Exception ex) {
             System.err.println(ex.getMessage());
         }
-    } 
-    
-    public void remove(){
-        try{
+    }
+
+    public void remove() {
+        try {
             getCiudadSession().remove(selectedCiudad);
             itemsCiudad = null;
-        } catch(Exception ex){
-            System.err.println(ex.getMessage());         
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
         }
     }
 }
